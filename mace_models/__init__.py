5aae9bb48f78b9fe820dec9abb1fd514996911fe
PythonFZ <fabian.zills@web.de>
2023-08-03 13:20:28 +0200

    provide public ANI trained MACE models

---
+++ mace_models/__init__.py
@@ -0,0 +1,14 @@
+import zntrack
+import torch 
+import functools
+
+class LoadModel(zntrack.Node):
+    model_path: str = zntrack.dvc.deps()
+
+    def run(self) -> None:
+        pass
+    
+    @functools.lru_cache()
+    def get_model(self) -> torch.nn.Module:
+        with self.state.fs.open(self.model_path, 'rb') as f:
+            return torch.load(f)
