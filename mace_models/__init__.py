f374ae5859078b83e937e1f74d1634f6b7f0fb1d
Rokas Elijošius <re344@cam.ac.uk>
2023-08-06 18:44:23 +0100

    add the reference data file

--- mace_models/__init__.py
+++ mace_models/__init__.py
@@ -1,6 +1,7 @@
 import zntrack
 import torch 
 import functools
+import ase.io as aio
 
 class LoadModel(zntrack.Node):
     model_path: str = zntrack.dvc.deps()
@@ -12,3 +13,13 @@ class LoadModel(zntrack.Node):
     def get_model(self, **kwargs) -> torch.nn.Module:
         with self.state.fs.open(self.model_path, 'rb') as f:
             return torch.load(f, **kwargs)
+
+class XYZReader(zntrack.Node):
+    data_path: str = zntrack.dvc.deps()
+
+    def run(self) -> None:
+        pass
+
+    def get_atoms(self):
+        with self.state.fs.open(self.data_path) as f:
+            return aio.read(f, format="extxyz", index=":")
