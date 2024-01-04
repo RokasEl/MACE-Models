815cedc643fdc56bbf86e3746359904312c38aa6
PythonFZ <fabian.zills@web.de>
2024-01-04 15:59:11 +0100

    linting

--- mace_models/__init__.py
+++ mace_models/__init__.py
@@ -1,23 +1,26 @@
 import zntrack
-import torch 
+import torch
 import functools
 import ase.io as aio
+
 
 class LoadModel(zntrack.Node):
     model_path: str = zntrack.dvc.deps()
 
     def run(self) -> None:
         pass
-    
+
     @functools.lru_cache()
     def get_model(self, **kwargs) -> torch.nn.Module:
-        with self.state.fs.open(self.model_path, 'rb') as f:
+        with self.state.fs.open(self.model_path, "rb") as f:
             if kwargs.get("device", None) is None:
                 return torch.load(f)
             else:
                 model = torch.load(f, map_location=kwargs["device"])
                 model.to(kwargs["device"])
                 return model
+
+
 class XYZReader(zntrack.Node):
     data_path: str = zntrack.dvc.deps()
 
