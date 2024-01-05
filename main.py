eb8307001d1ec97cb0d958d284deb90fd11f6a3a
PythonFZ <fabian.zills@web.de>
2024-01-05 11:03:33 +0100

    add medium spice

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ project = zntrack.Project()
 with project:
     LoadModel(model_path="data/ani500k_small_DFT.model", name="ani500k_small")
     LoadModel(model_path="data/qm9_and_spice_hydrogenation.model", name="hydromace")
-    
+    LoadModel(model_path="data/SPICE_sm_inv_neut_E0_swa.model", name="medium_spice")
+
     XYZReader(data_path="data/qm9_reference_data.xyz", name="reference_data")
+    XYZReader(data_path="data/qm9_reference_data_medium.xyz", name="qm9_medium_examples")
     XYZReader(data_path="data/zinc_fragments_difflinker.xyz", name="linker_examples")
 
 project.run()
